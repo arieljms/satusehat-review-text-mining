871ae7f00d9bfd0933c3f1513d987a73ec8cc58f
arieljms <arieljames695@gmail.com>
2026-05-10 13:52:29 +0700

    scrape satusehat playstore reviews, normalize slang words, and generate wordcloud

---
+++ src/scrapping.py
@@ -0,0 +1,16 @@
+from google_play_scraper import reviews, Sort
+import pandas as pd
+
+app_id = "com.telkom.tracencare"
+
+result, _ = reviews(
+    app_id,
+    lang="id",
+    country="id",
+    sort=Sort.NEWEST,
+    count=1500,
+)
+
+df = pd.DataFrame(result)
+df = df[["reviewId", "content", "score", "at"]]
+df.to_csv("data/data_raw_satusehat.csv", index=False)
